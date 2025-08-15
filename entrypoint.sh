557f7ab6943959abc5076f3ca99e42e7a9f81978
Radio90 <luisantoni2727@gmail.com>
2025-08-15 12:03:17 -0400

    Refactor(deploy): Remove automatic migration from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,6 @@
 # Abort on any error
 set -e
 
-# Apply database migrations
-echo "Applying database migrations..."
-flask db upgrade
-
 # Start the main application
 echo "Starting the application..."
 exec "$@"
